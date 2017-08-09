71e2c18819a629b7037e96c9e05eb2f2b863ae13
Kalyan Mekala <kalyan.mekala@pega.com>
2017-08-08 23:19:42 -0400

    Add files

---
+++ src/test/java/com/pega/poc/JenkinsPOC.java
@@ -0,0 +1,15 @@
+package com.pega.poc;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by mekak2 on 8/8/17.
+ */
+public class JenkinsPOC {
+
+    @Test
+    public void HelloWorld(){
+
+        System.out.println("Print...."+" Hello World");
+    }
+}
